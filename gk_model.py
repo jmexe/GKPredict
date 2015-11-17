52cab4788e989b035b38db780b6f3caa6ccc8ad8
Jmexe <mjia.zb@gmail.com>
2015-11-16 20:10:03 -0500

    修改模型 合并设置参数函数进初始化函数 增加re-fit功能

--- gk_model.py
+++ gk_model.py
@@ -8,38 +8,97 @@ from math import sqrt
 import numpy as np
 
 class GKModel(object):
-    def __init__(self, file_path="", delimiter='\t'):
+    """--------------Initial---------------------------"""
+    def __init__(self, s1, c1, s2, c2, total=-1, low_cut=200, h_total_prop=0.08, l_total_prop=0,  h_stop_corr=0, highest_score=0):
         """
         Initialize the model
         :param file_path: File path of the score data
         :param delimiter: The delimiter between each column
         :return:
         """
-        if file_path is not "":
-            self.file_path = file_path
+        self.given_total = total
+        self.total = total
+
+        self.s1 = s1
+        self.s2 = s2
+        self.c1 = c1
+        self.c2 = c2
+
 
         #The lowest cutting score, the score under 200 will not be considered
-        self.cut = 200
+        self.cut = low_cut
 
-        #Proportion of high score students
-        self.h_total_prop = 0.08
-        #The proportion of the students that can be eliminated
-        self.l_total_prop = 0
-        #Correction for high-score guassian model
-        self.h_stop_corr = 0
-        #Highest score
-        self.highest_score = 700
+        #Proportion of high score students, should be in range(0.005, 0.23)
+        if h_total_prop < 0:
+            self.h_total_prop = 0.005
+        elif h_total_prop > 0.25:
+            self.h_total_prop = 0.25
+        else:
+            self.h_total_prop = h_total_prop
 
-        self.delimiter = delimiter
+        #Proportion of high score students, in range(0~0.2)
+        if l_total_prop < 0:
+            self.l_total_prop = 0
+        elif l_total_prop > 0.2:
+            self.l_total_prop = 0.2
+        else:
+            self.l_total_prop = l_total_prop
 
-    def check_file(self):
-        """
-        Check whther the file path is defined
-        :return:
-        """
-        return hasattr(self, "file_path")
+        #Correct the model change score in range(-100, 100)
+        if h_stop_corr < -100:
+            self.h_stop_corr = h_stop_corr
+        elif h_stop_corr > 100:
+            self.h_stop_corr = h_stop_corr
+        else:
+            self.h_stop_corr = h_stop_corr
 
-    def load_data(self):
+        #Highest score, in range(650, 749)
+        if highest_score < 650:
+            self.highest_score = 650
+        elif highest_score > 749:
+            self.highest_score = 749
+        else:
+            self.highest_score  =  highest_score
+
+    def fit(self, file_path, delimiter='\t'):
+        self.load_data(file_path, delimiter)
+        self.estimate()
+
+    def re_fit(self, s1=-1, c1=-1, s2=-1, c2=-1, total=-1):
+        if self.check_model() is not True:
+            print "Estimate model first!"
+            return
+
+        old_total = self.total
+        if total > 0:
+            self.given_total = total
+            self.total = total
+
+
+        if s1 > 0:
+            self.s1 = s1
+
+        if s2 > 0:
+            self.s2 = s2
+
+        if c1 > 0:
+            self.c1 = c1
+        else:
+            self.c1 = (int) (self.c1 / float(old_total) * self.total)
+
+
+
+
+        if c2 > 0:
+            self.c2 = c2
+        else:
+            self.c2 = (int) (self.c2 / float(old_total) * self.total)
+
+
+        self.estimate()
+
+
+    def load_data(self, file_path, delimiter='\t'):
         """
         Given file path ,load the score data, the file should be in following format:
         1st column -- score
@@ -52,17 +111,13 @@ class GKModel(object):
         :param filename: file path
         :return: data
         """
-        if self.check_file() is not True:
-            print "No data to load"
-            return
-
-        file = open(self.file_path)
+        file = open(file_path)
 
         self.data = {}
         acc_cnt = 0
         for line in file:
             #Split the row
-            split = line.strip().split(self.delimiter)
+            split = line.strip().split(delimiter)
             #Get the score(1st column)
             score = (int)(split[0])
 
@@ -80,12 +135,9 @@ class GKModel(object):
         Plot the data
         :return:
         """
-        if self.check_file() is not True:
-            print "File path not defined!"
-            return
-
         if self.check_data() is not True:
-            self.load_data()
+            print "Please fit data first!"
+            return
 
         self.pts = []
 
@@ -164,8 +216,8 @@ class GKModel(object):
         :param num_bins:
         :return:
         """
-        if self.check_file() is not True:
-            print "File path not defined!"
+        if self.check_data() is not True:
+            print "Please fit data first!"
             return
 
         if self.check_model() is not True:
@@ -210,7 +262,9 @@ class GKModel(object):
                 y22.append(1 - self.data[pt][1] / (float) (self.total))
             else:
                 #If the score is not in the dict, use the nearest rank
-                if pt < self.cut:
+                if i == 0:
+                    y22.append(0)
+                elif pt < self.cut:
                     y22.append(0)
                 elif pt > 750:
                     y22.append(1)
@@ -240,10 +294,6 @@ class GKModel(object):
         :param score_arr: An array of scores
         :return: The estimated ranks and real ranks of the scores
         """
-        if self.check_file() is not True:
-            print "File path not defined!"
-            return
-
         if self.check_model() is not True:
             print "Models are not estimated yet!"
             return
@@ -251,7 +301,8 @@ class GKModel(object):
         ranks = self.estimate_rank_for_scores(score_arr)
         if hasattr(self, "data") is not True:
             #load data
-            self.load_data()
+            print "Data are not loaded yet!"
+            return
 
         real_ranks = []
         for score in score_arr:
@@ -263,6 +314,7 @@ class GKModel(object):
         return ranks, real_ranks
 
 
+    """--------------Estimation------------------------"""
 
     def estimate_guassian(self, s1, c1, s2, c2, total):
         """
@@ -309,73 +361,45 @@ class GKModel(object):
         Generating the score to rank table after estimating the model
         :return:
         """
-        if self.check_parameters():
-            self.a_miu, self.a_sig = self.all_score_estimate(self.s1, self.c1, self.s2, self.c2, self.total)
-            self.h_miu, self.h_sig = self.high_score_estimate(self.s1, self.highest_score, self.total * self.h_total_prop)
-            self.score_to_rank_table()
-        else:
-            print "Parameter are not all set!"
+        self.a_miu, self.a_sig = self.all_score_estimate(self.s1, self.c1, self.s2, self.c2, self.total)
+        self.h_miu, self.h_sig = self.high_score_estimate(self.s1, self.highest_score, self.total * self.h_total_prop)
+        self.sample_score_to_rank_table()
 
-    def set_parameters(self, s1, c1, s2, c2, total=-1, low_cut=200):
+
+    """--------------Check parameters------------------"""
+    def check_file(self):
         """
-        Set the score points for estimating the model
-        :param s1: 一本线
-        :param c1: rank of s1
-        :param s2: 二本线
-        :param c2: rank of s2
-        :param total: total students count, it's optional, if it's not given, must call the load data before starting the estimation
-        :param low_cut: The lowest cutting score, which means the score under that line will not be considered in the model, default value is 200
+        Check whther the file path is defined
         :return:
         """
-        self.s1 = s1
-        self.c1 = c1
-        self.s2 = s2
-        self.c2 = c2
+        return hasattr(self, "file_path")
 
-        self.given_total = total
-        self.total = total
-
-        self.cut = low_cut
-
-    def set_corrections(self, h_total_corr=0, l_total_corr=0,  h_stop_corr=0, highest_score_corr=0):
+    def check_data(self):
         """
-        Set the parameters for model correction
-        :param h_total:             Correction for the proportion of students modeled by the high-score guassian, default value is 8%
-        :param l_total_corr:        Correction for the proportion of the students that can be eliminated, default value is 0.
-        :param h_stop_corr:         Correction for the miu of the high-score guassian (used for changing model when estimate the rank)
-        :param highest_score_corr:  Correction for the highest score student, used for estimating the high-score guassian. Default value is 700
-        :return:
+        Check whether the data is loaded
         """
+        return hasattr(self, "data")
 
-        #Correct the high-score proportion in range(0.005, 0.23)
-        if h_total_corr < -0.075:
-            self.h_total_prop = 0.005
-        elif h_total_corr > 0.15:
-            self.h_total_prop = 0.23
+
+    def check_model(self):
+        """
+        Check whether the models are estimated
+        """
+        return hasattr(self, "a_miu") and hasattr(self, "a_sig") and hasattr(self, "h_miu") and hasattr(self, "h_sig")
+
+    def check_rank(self):
+        return hasattr(self, "prop_table")
+
+    """--------------Print parameters------------------"""
+    def print_model(self):
+        """
+        Print the parameters of the guassian models
+        """
+        if self.check_model():
+            print "Overall guassian parameters -- miu:", self.a_miu, " sigma:", self.a_sig
+            print "High-score guassian parameters -- miu:", self.h_miu, " sigma:", self.h_sig
         else:
-            self.h_total_prop += h_total_corr
-
-        #Correct the low-score proportion in range(0~0.2)
-        if l_total_corr < 0:
-            self.l_total_prop = 0
-        elif l_total_corr > 0.2:
-            self.l_total_prop = 0.2
-        else:
-            self.l_total_prop += l_total_corr
-
-        #Correct the model change score in range(-100, 100)
-        if h_stop_corr < -100:
-            self.h_stop_corr = h_stop_corr
-        elif h_stop_corr > 100:
-            self.h_stop_corr = h_stop_corr
-
-        #Correct the highest score in range(-50, 50)
-        if highest_score_corr < -50:
-            self.highest_score = 650
-        elif highest_score_corr > 50:
-            self.highest_score = 749
-        else:
-            self.highest_score  +=  highest_score_corr
+            print "Models are not estimated yet!"
 
 
     def print_parameters(self):
@@ -389,41 +413,8 @@ class GKModel(object):
 
         print "修正值 -- 最高分:", self.highest_score, " 高分人数比例:", self.h_total_prop, "低分人数比例:", self.l_total_prop, "模型切换分数线修正:", self.h_stop_corr
 
-    def check_data(self):
-        """
-        Check whether the data is loaded
-        """
-        return hasattr(self, "data")
-
-    def check_parameters(self):
-        """
-        Check whether all the required parameters are all set
-        """
-        if hasattr(self, "s1") and hasattr(self, "c1") and hasattr(self, "s2") and hasattr(self, "c2") and hasattr(self, "total"):
-            return self.total > 0
-        else:
-            return False
-
-    def check_model(self):
-        """
-        Check whether the models are estimated
-        """
-        return hasattr(self, "a_miu") and hasattr(self, "a_sig") and hasattr(self, "h_miu") and hasattr(self, "h_sig")
-
-    def check_rank(self):
-        return hasattr(self, "prop_table")
-
-    def print_model(self):
-        """
-        Print the parameters of the guassian models
-        """
-        if self.check_model():
-            print "Overall guassian parameters -- miu:", self.a_miu, " sigma:", self.a_sig
-            print "High-score guassian parameters -- miu:", self.h_miu, " sigma:", self.h_sig
-        else:
-            print "Models are not estimated yet!"
-
-    def score_to_rank_table(self, total=-1):
+    """--------------Predict score/rank data-----------"""
+    def sample_score_to_rank_table(self, total=-1):
         """
         Create the score-to-rank table and the score-to-proportion table
         Return the score-to-rank table
@@ -452,19 +443,17 @@ class GKModel(object):
         return score_to_rank_table
 
 
-
-    def rank_to_score(self, rank, total=-1):
+    def get_rank_to_score(self, rank, total=-1):
         """
-        Given rank ,estimate the score
+        Given rank ,get the score
         """
-
 
         if self.check_model() is not True:
             print "Models are not estimated yet!"
             return
 
         if self.check_rank() is not True:
-            self.score_to_rank_table()
+            self.sample_score_to_rank_table()
 
         if total < 0:
             total = self.total
@@ -496,117 +485,32 @@ class GKModel(object):
         #return (int) (self.prop_table[score] * total)
 
 if __name__ == '__main__':
-    #三个例程, 删掉注释可用
-
-
 
     """ ---------------------------2015 山东文科数据----------------------------------"""
-    """
-    #初始化模型
-    #数据文件路径 -- 数据格式在代码中有注释
-    #省份、年份、文理科
-    model = GKModel("./data/sd-2015-w.txt", "山东", 2015, "文科")
+    #-----------------初始化模型--------------------#
+    #必须给定的参数：
+    #一本线， 一本线排名， 二本线，二本线排名 -- (568, 21243, 510, 53556)
+    #总人数可以不给出，如果给定，可以直接调用estimate()函数去估计参数，不然必须通过fit()函数去读取文件，同时estimate
+    #low_cut是最低分数线，低于此分数线的分数不予考虑，默认值是200
+    #h_total_prop   --  高分考生比例，默认值0.08，范围(0, 0.25)
+    #l_total_prop   --  低分考生比例，默认值0, 范围(0, 0.2)
+    #h_stop_corr    --  切换模型分数线修正值, 修正范围(-100, 100)，默认为高分高斯的miu值，此处设置为-10， 则模型中模型切换的分数线为 h_miu - 10
+    #highest_score  --  最高分，默认是700，范围(650, 749)
+    model = GKModel(516, 13556, 439, 27275, total=-1, low_cut=200, h_total_prop=0.08, l_total_prop=0,  h_stop_corr=-10, highest_score=705)
+    #Fit数据，读取数据，重新计算总人数，fit完后可以调用show_model()函数输出图表
+    model.fit("./data/tj-2014-l.txt.csv",delimiter='\t')
+    model.show_model(200)
+    #print model.compare_data([500, 600])
+    #print model.sample_score_to_rank_table()
 
-    #设置参数
-    #一本线， 一本线排名， 二本线，二本线排名， 考生总人数
-    model.set_parameters(568, 21243, 510, 53556, 138374)
-
-    #设置修正值(optional)
-    #此处设置的是修正值，都是对模型中参数进行修正
-    #模型中默认参数：高分考生比例8%, 模型切换分数是高分高斯的miu值，低分考生比例为0，考生最高分为700
-    #
-    #h_total_corr:       高分人数修正值，修正范围(-0.08, 0.15)，系统默认值0.08，此处设为0，则模型中高分考生比例为 8%+0 = 8%
-    #h_stop_corr:        切换模型分数线修正值, 修正范围(-100, 100)，默认为高分高斯的miu值，此处设置为-10， 则模型中模型切换的分数线为 h_miu - 10
-    #l_total_corr:       低分考生比例修正值，修正范围(0, 0.3)，系统默认值0，此处设为0，则模型中高分考生比例为 0%+0 = 0%
-    #highest_score_corr: 考生最高分修正值，修正范围(-50, 50)，系统默认值为700，此处设为0，则模型中最高分考生为700 + 0 = 700
-    model.set_corrections(h_total_corr=0, h_stop_corr=-10, l_total_corr=0, highest_score_corr=5)
-
-    #根据输入数据估计模型
-    model.estimate()
-
-    #测试模型
-    #给定一组分数，估计排名
-    test_scores = [500, 540, 580, 620, 660, 700, 720]
-    estimate_ranks, real_ranks = model.compare_data(test_scores)
-    for i, score in enumerate(test_scores):
-        print score, "-- est rank:", estimate_ranks[i], "-- real rank:", real_ranks[i]
-
-    #画图,参数为x轴坐标密度
-    model.show_model(100)
-    """
-
-    """ ---------------------------2015 山东理科数据----------------------------------"""
-
-    """
-    #Initialize the model
-    model = GKModel("./data/sd-2015-l.txt", "山东", 2015, "理科")
-
-    #Set the parameters
-    model.set_parameters(562, 80062, 490, 150651, 257728)
-    #Set the correction parameters
-    model.set_corrections(h_total_corr=0, h_stop_corr=50, l_total_corr=0, highest_score_corr=40)
-    #Estimate the model
-    model.estimate()
-    #Given an array of scores, compare the estimate ranks with real ranks
-    test_scores = [500, 540, 580, 620, 660, 700, 720]
-    estimate_ranks, real_ranks = model.compare_data(test_scores)
-    for i, score in enumerate(test_scores):
-        print score, "-- est rank:", estimate_ranks[i], "-- real rank:", real_ranks[i]
-    #Plot the model
-    model.show_model(100)
-    """
+    #Re-fit函数，通过往年模型，给定当年部分参数，重新估计当年参数
+    #s1 -- 一本线，默认采用往年一本线
+    #c1 -- 一本排名，默认采用 往年一本线所占考生比例 * 当年总人数
+    #s2 -- 二本线，默认采用往年二本线
+    #c2 -- 二本排名，默认采用 往年二本线所占考生比例 * 当年总人数
+    #total -- 总人数，默认值会采用往年总人数
+    #model.re_fit(s1=550, s2=500)
 
 
-    """ ---------------------------2015 广西理科数据----------------------------------"""
-    """
-    #Initialize the model
-    model = GKModel("./data/gx-2015-l.txt", "广西", 2015, "理科")
 
-    #Set the parameters
-    model.set_parameters(480, 25123, 320, 97807, 145318)
-    #Set the correction parameters
-    model.set_corrections(h_total_corr=0, h_stop_corr=0, l_total_corr=0, highest_score_corr=0)
-    #Estimate the model
-    model.estimate()
-    #Given an array of scores, compare the estimate ranks with real ranks
-    test_scores = [500, 540, 580, 620, 660, 700, 720]
-    estimate_ranks, real_ranks = model.compare_data(test_scores)
-    for i, score in enumerate(test_scores):
-        print score, "-- est rank:", estimate_ranks[i], "-- real rank:", real_ranks[i]
-    #Plot the model
-    model.show_model(100)
-    """
-
-    """ ---------------------------2015 广西理科数据----------------------------------"""
-    """
-    #Initialize the model
-    model = GKModel("./data/yn-2014-l.txt", "云南", 2014, "理科")
-
-    #Set the parameters
-    model.set_parameters(525, 21363, 445, 54007, 124963)
-
-    model.load_data()
-    model.show_data(100)
-
-    #Set the correction parameters
-    model.set_corrections(h_total_corr=0, h_stop_corr=39, l_total_corr=0, highest_score_corr=43)
-    #Estimate the model
-    model.estimate()
-    #Given an array of scores, compare the estimate ranks with real ranks
-    test_scores = [220, 500, 540, 580, 620, 660, 700, 720]
-    estimate_ranks, real_ranks = model.compare_data(test_scores)
-    for i, score in enumerate(test_scores):
-        print score, "-- est rank:", estimate_ranks[i], "-- real rank:", real_ranks[i]
-    #Plot the model
-    model.show_model(100)
-    """
-
-    """ ---------------------------2015 内蒙文科数据----------------------------------"""
-    #Initialize the model
-    model = GKModel(file_path = "./data/neimenggu-2015-w.csv", delimiter=',')
-    #Set the parameters
-    model.set_parameters(525, 4755, 455, 14273, total=50469)
-    #Set the correction parameters
-    model.set_corrections(h_total_corr=-0.06, h_stop_corr=0, l_total_corr=0.1, highest_score_corr=-30)
-    #Estimate the model
-    model.estimate()
+    #print model.sample_score_to_rank_table()
